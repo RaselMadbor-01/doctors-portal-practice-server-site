ac19e42b36daa8fc4a8cc03db88ab0946173d953
RaselMadbor-01 <md.raselmadbor01311211424@gmail.com>
2021-11-17 10:55:14 +0600

    some changes

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
     //GET
     app.get("/appointment", verifyToken,async (req, res) => {
       const email = req.query.email;
-      const date = new Date(req.query.date).toLocaleDateString();
+      const date = req.query.date;
       const query = { email: email, date: date };
       const cursor = appointmentCollection.find(query);
       const result = await cursor.toArray();
